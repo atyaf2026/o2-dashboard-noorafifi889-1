759ccd02d297c9671df60c5b8985140286651839
noorafifi889 <noorelafifi889@gmail.com>
2026-05-04 20:47:31 +0300

    add a mock users in typescript file

---
+++ src/components/ui/Users/MockUsers.ts
@@ -0,0 +1,52 @@
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  role: 'Admin' | 'Student'; 
+  status: 'Active' | 'Inactive' | 'Pending';
+  joinDate: string;
+}
+
+
+export const mockUsers = [
+  { 
+    id: '1', 
+    name: 'Rivera', 
+    email: 'rivera@o2dashboard.com', 
+    role: 'Admin', 
+    status: 'Active', 
+    joinDate: 'Oct 24, 2023' 
+  },
+  { 
+    id: '2', 
+    name: 'Chen', 
+    email: 'c.c@o2dashboard.com', 
+    role: 'Student', 
+    status: 'Active', 
+    joinDate: 'Nov 12, 2023' 
+  },
+  { 
+    id: '3', 
+    name: 'Knight', 
+    email: 'ight@o2dashboard.com', 
+    role: 'Student', 
+    status: 'Inactive', 
+    joinDate: 'Dec 21, 2023' 
+  },
+  { 
+    id: '4', 
+    name: 'Smyth', 
+    email: 'n.s@o2dashboard.com', 
+    role: 'Student', 
+    status: 'Pending', 
+    joinDate: 'Jan 14, 2024' 
+  },
+  { 
+    id: '5', 
+    name: 'Rodriguez', 
+    email: 'r@o2dashboard.com', 
+    role: 'Admin', 
+    status: 'Active', 
+    joinDate: 'Feb 22, 2024' 
+  }
+];
