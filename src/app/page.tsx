8ba5da50e6228ea13760875e317cb4fbd77ea268
noorafifi889 <noorelafifi889@gmail.com>
2026-05-04 20:48:27 +0300

    remove  padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import HomePage from '../components/ui/Home/HomePage';
 
 export default function Home() {
   return (
-    <main className="min-h-screen p-4 md:p-5 lg:p-6">
+    <main className="min-h-screen  md:p-5 lg:p-6">
       <div className="max-w-[1600px] mx-auto">
         <HomePage />
       </div>
